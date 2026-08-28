f412a33b8a6acd455a685291e0324514e9235b09
Bhushan pawar <bapawar9409@gmail.com>
2026-08-28 21:07:12 +0530

    set cross origin

--- src/main/java/com/shoppingcart/configuration/CorsConfig.java
+++ src/main/java/com/shoppingcart/configuration/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.shoppingcart.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+	@Value("${cors.allowed-origin}")
+    private String allowedOrigin;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -15,10 +18,16 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+            	 registry.addMapping("/**")
+                 .allowedOrigins(allowedOrigin)
+                 .allowedMethods(
+                         "GET",
+                         "POST",
+                         "PUT",
+                         "DELETE",
+                         "OPTIONS"
+                 )
+                 .allowedHeaders("*");
             }
         };
     }
